1991038cf6ab2764fb0916a89d41c67ad31c1a10
Agni Agni <y4nm45@gmail.com>
2025-07-20 20:06:46 +0100

    updated file exercises/f5567c1e-5f7e-423c-a99e-28b75a5e28ee/solutions/2b68e3de-c126-46b6-8aa4-7f70b2339809/sol.py

--- exercises/f5567c1e-5f7e-423c-a99e-28b75a5e28ee/solutions/2b68e3de-c126-46b6-8aa4-7f70b2339809/sol.py
+++ exercises/f5567c1e-5f7e-423c-a99e-28b75a5e28ee/solutions/2b68e3de-c126-46b6-8aa4-7f70b2339809/sol.py
@@ -2,16 +2,16 @@
  *              DO NOT TOUCH AREA: START                *
  ********************************************************/
 
-function main() {
-    const input = readln();
-    writeln(exercise(input));
+def main() {
+    n = float(input())
+    print(exercise(input));
 }
 
 /********************************************************
  *                DO NOT TOUCH AREA: END                *
  ********************************************************/
 
-function exercise(input) {
+def exercise(input) {
 	if temp_rise > 1.5:
         print("Danger")
     else:
