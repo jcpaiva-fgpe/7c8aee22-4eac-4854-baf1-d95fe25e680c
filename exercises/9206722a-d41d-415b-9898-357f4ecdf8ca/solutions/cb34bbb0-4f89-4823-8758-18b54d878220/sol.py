4c8a38f79f36ee4f9d9c5b548e6b8ad56f0d5964
Agni Agni <y4nm45@gmail.com>
2025-07-20 19:52:26 +0100

    created file exercises/9206722a-d41d-415b-9898-357f4ecdf8ca/solutions/cb34bbb0-4f89-4823-8758-18b54d878220/sol.py

---
+++ exercises/9206722a-d41d-415b-9898-357f4ecdf8ca/solutions/cb34bbb0-4f89-4823-8758-18b54d878220/sol.py
@@ -0,0 +1,18 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+function main() {
+    writeln(exercise());
+}
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+
+function exercise() {
+	cities = ["Berlin", "Rome", "Madrid"]
+    warming = (0.9, 1.3, 1.7)
+    city_data = {"Berlin": 0.9, "Rome": 1.3, "Madrid": 1.7}
+    print(cities, warming, city_data)
+}
