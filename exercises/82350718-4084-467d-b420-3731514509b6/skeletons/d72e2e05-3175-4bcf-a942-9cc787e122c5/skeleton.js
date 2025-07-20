25d1c0abacb3ead22f019eb720ecddd5efcd3d57
Agni Agni <y4nm45@gmail.com>
2025-07-20 19:47:06 +0100

    created file exercises/82350718-4084-467d-b420-3731514509b6/skeletons/d72e2e05-3175-4bcf-a942-9cc787e122c5/skeleton.js

---
+++ exercises/82350718-4084-467d-b420-3731514509b6/skeletons/d72e2e05-3175-4bcf-a942-9cc787e122c5/skeleton.js
@@ -0,0 +1,17 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+function main() {
+    writeln(exercise);
+}
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+
+function exercise() {
+	city = "Berlin"
+    local_rise = 0.9
+    print(city, " has warmed by ", local_rise, " °C")
+}
