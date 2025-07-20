3c961043b0aacc111ffd904caa40db6562d0a79c
Agni Agni <y4nm45@gmail.com>
2025-07-20 19:34:42 +0100

    created file exercises/095a60ce-24c7-40d3-869f-c9f24862fd5c/solutions/753764ad-58b7-4c25-8983-f094d856d22a/sol.py

---
+++ exercises/095a60ce-24c7-40d3-869f-c9f24862fd5c/solutions/753764ad-58b7-4c25-8983-f094d856d22a/sol.py
@@ -0,0 +1,15 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+function main() {
+    writeln(exercise());
+}
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+
+function exercise() {
+	print("Global average temperature has risen 1.2 °C since pre‑industrial times!")
+}
