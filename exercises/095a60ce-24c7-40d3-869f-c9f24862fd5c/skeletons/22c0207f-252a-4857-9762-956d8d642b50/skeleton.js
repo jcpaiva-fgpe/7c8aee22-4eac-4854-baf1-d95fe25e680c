a1be04771db9955fb773cc97f87cb7ad1f7c92c3
Agni Agni <y4nm45@gmail.com>
2025-07-20 19:33:48 +0100

    created file exercises/095a60ce-24c7-40d3-869f-c9f24862fd5c/skeletons/22c0207f-252a-4857-9762-956d8d642b50/skeleton.js

---
+++ exercises/095a60ce-24c7-40d3-869f-c9f24862fd5c/skeletons/22c0207f-252a-4857-9762-956d8d642b50/skeleton.js
@@ -0,0 +1,16 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+function main() {
+    const n = readln();
+    writeln(exercise(n));
+}
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+
+function exercise(n) {
+	// TODO this is what you need to complete
+}
