93e0b33fe2f5ee712570693da2b782ab147195f5
Agni Agni <y4nm45@gmail.com>
2025-07-20 20:01:37 +0100

    created file exercises/f5567c1e-5f7e-423c-a99e-28b75a5e28ee/solutions/2b68e3de-c126-46b6-8aa4-7f70b2339809/sol.py

---
+++ exercises/f5567c1e-5f7e-423c-a99e-28b75a5e28ee/solutions/2b68e3de-c126-46b6-8aa4-7f70b2339809/sol.py
@@ -0,0 +1,19 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+function main() {
+    const input = readln();
+    writeln(exercise(input));
+}
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+
+function exercise(input) {
+	if temp_rise > 1.5:
+        print("Danger")
+    else:
+        print("Within limits")
+}
