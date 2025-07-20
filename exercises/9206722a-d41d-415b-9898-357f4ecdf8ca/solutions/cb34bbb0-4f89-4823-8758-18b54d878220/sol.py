d825c4eca8ed57556dda71a4d7b1cb29819f116a
Agni Agni <y4nm45@gmail.com>
2025-07-20 20:10:46 +0100

    updated file exercises/9206722a-d41d-415b-9898-357f4ecdf8ca/solutions/cb34bbb0-4f89-4823-8758-18b54d878220/sol.py

--- exercises/9206722a-d41d-415b-9898-357f4ecdf8ca/solutions/cb34bbb0-4f89-4823-8758-18b54d878220/sol.py
+++ exercises/9206722a-d41d-415b-9898-357f4ecdf8ca/solutions/cb34bbb0-4f89-4823-8758-18b54d878220/sol.py
@@ -2,17 +2,17 @@
  *              DO NOT TOUCH AREA: START                *
  ********************************************************/
 
-function main() {
-    writeln(exercise());
+def main() {
+    print(exercise());
 }
 
 /********************************************************
  *                DO NOT TOUCH AREA: END                *
  ********************************************************/
 
-function exercise() {
+def exercise() {
 	cities = ["Berlin", "Rome", "Madrid"]
     warming = (0.9, 1.3, 1.7)
     city_data = {"Berlin": 0.9, "Rome": 1.3, "Madrid": 1.7}
-    print(cities, warming, city_data)
+    return cities + warming + city_data
 }
