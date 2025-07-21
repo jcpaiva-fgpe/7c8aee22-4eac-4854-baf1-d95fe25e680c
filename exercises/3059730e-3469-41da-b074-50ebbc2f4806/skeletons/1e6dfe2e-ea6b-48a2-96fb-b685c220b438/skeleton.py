39d43941e79e3e97ef3b4fd5bd88139774f28005
Agni Agni <y4nm45@gmail.com>
2025-07-21 14:39:09 +0100

    created file exercises/3059730e-3469-41da-b074-50ebbc2f4806/skeletons/1e6dfe2e-ea6b-48a2-96fb-b685c220b438/skeleton.py

---
+++ exercises/3059730e-3469-41da-b074-50ebbc2f4806/skeletons/1e6dfe2e-ea6b-48a2-96fb-b685c220b438/skeleton.py
@@ -0,0 +1,10 @@
+############## DO NOT TOUCH AREA: START #################
+
+if __name__ == '__main__':
+    broadcast()
+
+############## DO NOT TOUCH AREA: END ###################
+
+
+def broadcast():
+    # TODO this is what you need to complete
