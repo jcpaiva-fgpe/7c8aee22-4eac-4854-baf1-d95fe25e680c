2f3db618de6820d838edf7dd96761c843c5cddc4
Agni Agni <y4nm45@gmail.com>
2025-07-21 14:08:58 +0100

    created file exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/solutions/25c7faaf-840b-460c-a0c3-a20784b604a6/sol.py

---
+++ exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/solutions/25c7faaf-840b-460c-a0c3-a20784b604a6/sol.py
@@ -0,0 +1,11 @@
+############## DO NOT TOUCH AREA: START #################
+
+if __name__ == '__main__':
+    broadcast()
+
+############## DO NOT TOUCH AREA: END ###################
+
+
+def broadcast():
+    # TODO this is what you need to complete
+    print("Global average temperature has risen 1.2 °C since pre‑industrial times!")
