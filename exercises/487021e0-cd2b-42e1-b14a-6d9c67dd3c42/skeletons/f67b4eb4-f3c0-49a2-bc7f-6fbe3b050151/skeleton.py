17fb50c2063ea7df63a9e685609b2998de3186a2
Agni Agni <y4nm45@gmail.com>
2025-07-21 09:36:30 +0100

    created file exercises/487021e0-cd2b-42e1-b14a-6d9c67dd3c42/skeletons/f67b4eb4-f3c0-49a2-bc7f-6fbe3b050151/skeleton.py

---
+++ exercises/487021e0-cd2b-42e1-b14a-6d9c67dd3c42/skeletons/f67b4eb4-f3c0-49a2-bc7f-6fbe3b050151/skeleton.py
@@ -0,0 +1,16 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+if __name__ == '__main__':
+    portfolio = input()
+    coin = input()
+    print(time_to_buy(portfolio, coin))
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+    
+# TODO this is what you need to complete
+
+    
