8bef926146f53f7c3377f147a75495b6e42065d1
Agni Agni <y4nm45@gmail.com>
2025-07-21 09:32:24 +0100

    created file exercises/b81866ae-a115-48a7-bde3-5aa7133ff1c5/skeletons/fba8c0c4-101e-4112-a828-08ac77145d2a/skeleton.py

---
+++ exercises/b81866ae-a115-48a7-bde3-5aa7133ff1c5/skeletons/fba8c0c4-101e-4112-a828-08ac77145d2a/skeleton.py
@@ -0,0 +1,16 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+if __name__ == '__main__':
+    wallet = input()
+    print(portfolio_summary(wallet))
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+    
+def portfolio_summary(wallet):
+    # TODO this is what you need to complete
+
+    
