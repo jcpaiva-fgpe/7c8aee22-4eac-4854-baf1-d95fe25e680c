1c14c7b7c64a9d556880a2971ea0143f43f948a4
Agni Agni <y4nm45@gmail.com>
2025-07-21 08:22:11 +0100

    created file exercises/739ad0cf-fe9d-4e12-87f4-0d3c0835b8d1/skeletons/38dc70f9-29af-46b7-bd02-250d1c01bea8/skeleton.py

---
+++ exercises/739ad0cf-fe9d-4e12-87f4-0d3c0835b8d1/skeletons/38dc70f9-29af-46b7-bd02-250d1c01bea8/skeleton.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+if __name__ == '__main__':
+    seed = input
+    print(exercise(seed))
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+    
+def exercise(seed):
+    # TODO this is what you need to complete
+
+    
