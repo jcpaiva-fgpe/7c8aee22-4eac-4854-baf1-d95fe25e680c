ea47b07bbfd45ce09cd4c5eb3a5bd4ed97edf11f
Agni Agni <y4nm45@gmail.com>
2025-07-21 08:51:41 +0100

    created file exercises/05024546-3417-4088-bcf9-cf3fc8bfabd3/solutions/fabaa85e-794d-470f-b5dc-3f081240937d/sol.py

---
+++ exercises/05024546-3417-4088-bcf9-cf3fc8bfabd3/solutions/fabaa85e-794d-470f-b5dc-3f081240937d/sol.py
@@ -0,0 +1,17 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+if __name__ == '__main__':
+    print(exercise())
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+    
+def exercise():
+    # TODO this is what you need to complete
+    my_coins = ["BTC", "ETH", "SOL"]
+    wallet = {"BTC": 0.05, "ETH": 0.3, "SOL": 10}
+    return my_coins, wallet
+    
