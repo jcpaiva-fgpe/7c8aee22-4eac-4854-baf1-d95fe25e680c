73085dd69ffafc9220510b8fe4e6574b7084cade
Agni Agni <y4nm45@gmail.com>
2025-07-21 09:14:40 +0100

    created file exercises/b76f88cd-7f12-4a3e-b7fe-5e6f70e6136b/skeletons/5a02df95-1c90-4318-b926-3c9d53aba5a9/skeleton.py

---
+++ exercises/b76f88cd-7f12-4a3e-b7fe-5e6f70e6136b/skeletons/5a02df95-1c90-4318-b926-3c9d53aba5a9/skeleton.py
@@ -0,0 +1,17 @@
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+if __name__ == '__main__':
+    entry = input()
+    current = input()
+    print(exercise(entry, current))
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+    
+def exercise(entry, current):
+    # TODO this is what you need to complete
+
+    
