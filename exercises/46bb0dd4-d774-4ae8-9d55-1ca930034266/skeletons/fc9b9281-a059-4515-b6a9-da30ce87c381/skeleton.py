276a944d3e8329e36eb986c8abd679c5ae30ff27
Agni Agni <y4nm45@gmail.com>
2025-07-21 08:17:59 +0100

    created file exercises/46bb0dd4-d774-4ae8-9d55-1ca930034266/skeletons/fc9b9281-a059-4515-b6a9-da30ce87c381/skeleton.py

---
+++ exercises/46bb0dd4-d774-4ae8-9d55-1ca930034266/skeletons/fc9b9281-a059-4515-b6a9-da30ce87c381/skeleton.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+/********************************************************
+ *              DO NOT TOUCH AREA: START                *
+ ********************************************************/
+
+if __name__ == '__main__':
+    array1 = input()
+    array2 = input()
+    print(exercise(array1, array2))
+
+/********************************************************
+ *                DO NOT TOUCH AREA: END                *
+ ********************************************************/
+    
+def exercise(array1, array2):
+    # TODO this is what you need to complete
+
+    
