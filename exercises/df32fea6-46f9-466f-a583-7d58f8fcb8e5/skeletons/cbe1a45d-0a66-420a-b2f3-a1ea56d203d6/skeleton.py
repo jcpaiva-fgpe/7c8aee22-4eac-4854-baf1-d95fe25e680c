6bfa54180ab8c6822b2e22a6b5469f83def667e9
Agni Agni <y4nm45@gmail.com>
2025-07-28 08:18:49 +0100

    updated file exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/skeletons/cbe1a45d-0a66-420a-b2f3-a1ea56d203d6/skeleton.py

--- exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/skeletons/cbe1a45d-0a66-420a-b2f3-a1ea56d203d6/skeleton.py
+++ exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/skeletons/cbe1a45d-0a66-420a-b2f3-a1ea56d203d6/skeleton.py
@@ -1,10 +1,11 @@
+def broadcast():
+    # TODO this is what you need to complete
+
+
+
 ############## DO NOT TOUCH AREA: START #################
 
 if __name__ == '__main__':
     broadcast()
 
 ############## DO NOT TOUCH AREA: END ###################
-
-
-def broadcast():
-    # TODO this is what you need to complete
