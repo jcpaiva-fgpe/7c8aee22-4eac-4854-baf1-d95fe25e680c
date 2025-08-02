b042fd48d5588546c593cae93fc2255d5728d474
Agni Agni <y4nm45@gmail.com>
2025-08-02 20:51:33 +0100

    created file exercises/133a9874-d5f1-49de-b2e0-7f761dae8cb8/skeletons/c5baebc3-f69c-4eaf-adba-711155caffe8/skeleton.py

---
+++ exercises/133a9874-d5f1-49de-b2e0-7f761dae8cb8/skeletons/c5baebc3-f69c-4eaf-adba-711155caffe8/skeleton.py
@@ -0,0 +1,12 @@
+def sell_or_not(entry_value, current_value):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    entry_value = int(inp.split("0")[0])
+    current_value = int(inp.split("0")[1])
+    sell_or_not(entry_value, current_value)
+############## DO NOT TOUCH AREA: END ###################
