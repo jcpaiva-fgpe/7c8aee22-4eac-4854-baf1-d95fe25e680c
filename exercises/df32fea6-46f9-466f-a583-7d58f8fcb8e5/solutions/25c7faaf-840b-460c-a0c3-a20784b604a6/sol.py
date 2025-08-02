e44cde209af7c0cc01192c687a95b7b6c8c0b39f
Agni Agni <y4nm45@gmail.com>
2025-08-02 19:05:59 +0100

    Removed exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/solutions/25c7faaf-840b-460c-a0c3-a20784b604a6/sol.py

--- exercises/df32fea6-46f9-466f-a583-7d58f8fcb8e5/solutions/25c7faaf-840b-460c-a0c3-a20784b604a6/sol.py
+++
@@ -1,9 +0,0 @@
-def broadcast():
-    # TODO this is what you need to complete
-    print("Global average temperature has risen 1.2 °C since pre‑industrial times!")
-
-
-############## DO NOT TOUCH AREA: START #################
-if __name__ == '__main__':
-    broadcast()
-############## DO NOT TOUCH AREA: END ###################
