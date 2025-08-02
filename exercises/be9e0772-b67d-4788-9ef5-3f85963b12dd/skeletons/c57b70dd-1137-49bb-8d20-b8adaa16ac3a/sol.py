d9f29b435f96891a16c23015a791d9ea6133e7db
Agni Agni <y4nm45@gmail.com>
2025-08-02 21:22:37 +0100

    created file exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/skeletons/c57b70dd-1137-49bb-8d20-b8adaa16ac3a/sol.py

---
+++ exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/skeletons/c57b70dd-1137-49bb-8d20-b8adaa16ac3a/sol.py
@@ -0,0 +1,11 @@
+import json
+# TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    data = json.loads(inp)
+    portfolio(data)
+############## DO NOT TOUCH AREA: END ###################
