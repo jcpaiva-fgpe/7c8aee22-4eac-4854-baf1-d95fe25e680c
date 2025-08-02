3bef1cd828281117ce504f9383b670db79fc29fe
Agni Agni <y4nm45@gmail.com>
2025-08-02 20:01:05 +0100

    created file exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/solutions/20f265ee-fea8-4412-a59a-c1f5101b1407/sol.py

---
+++ exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/solutions/20f265ee-fea8-4412-a59a-c1f5101b1407/sol.py
@@ -0,0 +1,11 @@
+def wallet():
+    # TODO this is what you need to complete
+    my_coins = ["BTC", "ETH", "SOL"]
+    wallet = {"BTC": 0.05, "ETH": 0.3, "SOL": 10}
+    print(my_coins, wallet)
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    wallet()
+############## DO NOT TOUCH AREA: END ###################
