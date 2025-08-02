cba35d0f365d12d8573a9dff7692bb4928a25d77
Agni Agni <y4nm45@gmail.com>
2025-08-02 21:22:48 +0100

    created file exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/solutions/14b66238-1e84-4cb5-ada2-0c9446686922/sol.py

---
+++ exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/solutions/14b66238-1e84-4cb5-ada2-0c9446686922/sol.py
@@ -0,0 +1,15 @@
+import json
+# TODO this is what you need to complete
+def portfolio(coins):
+    total = 0
+    for price in coins.values():
+        total += price
+    return total
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    data = json.loads(inp)
+    portfolio(data)
+############## DO NOT TOUCH AREA: END ###################
