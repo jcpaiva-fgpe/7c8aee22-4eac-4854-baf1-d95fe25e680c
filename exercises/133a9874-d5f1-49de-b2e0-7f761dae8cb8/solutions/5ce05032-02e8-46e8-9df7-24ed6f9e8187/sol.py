54c0fdd890a521b5615c57473014423d558104ee
Agni Agni <y4nm45@gmail.com>
2025-08-02 20:52:09 +0100

    created file exercises/133a9874-d5f1-49de-b2e0-7f761dae8cb8/solutions/5ce05032-02e8-46e8-9df7-24ed6f9e8187/sol.py

---
+++ exercises/133a9874-d5f1-49de-b2e0-7f761dae8cb8/solutions/5ce05032-02e8-46e8-9df7-24ed6f9e8187/sol.py
@@ -0,0 +1,16 @@
+def sell_or_not(entry_value, current_value):
+    # TODO this is what you need to complete
+    gain = (current_value - entry_value) / entry_value * 100
+    if gain > 50:
+        print("SELL!")
+    else:
+        print("HOLD")
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    entry_value = int(inp.split("0")[0])
+    current_value = int(inp.split("0")[1])
+    sell_or_not(entry_value, current_value)
+############## DO NOT TOUCH AREA: END ###################
