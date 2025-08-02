2a27d5ffbafe35a7fb004f3eca6383149947ed13
Agni Agni <y4nm45@gmail.com>
2025-08-02 19:33:26 +0100

    created file exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/skeletons/b7426680-137e-422c-a99e-04a8f14b2b5f/skeleton.py

---
+++ exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/skeletons/b7426680-137e-422c-a99e-04a8f14b2b5f/skeleton.py
@@ -0,0 +1,9 @@
+def coin():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    coin()
+############## DO NOT TOUCH AREA: END ###################
