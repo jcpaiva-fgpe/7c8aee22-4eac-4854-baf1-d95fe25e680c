16fec6ab6d63a858e8669ce48a759f92e07e229b
Agni Agni <y4nm45@gmail.com>
2025-08-02 21:09:06 +0100

    created file exercises/898add38-bf74-41f1-bb3f-7246da23b87c/skeletons/01695a41-cc9f-49d8-88ae-7b0b66f08813/skeleton.py

---
+++ exercises/898add38-bf74-41f1-bb3f-7246da23b87c/skeletons/01695a41-cc9f-49d8-88ae-7b0b66f08813/skeleton.py
@@ -0,0 +1,13 @@
+import json
+
+def check(wallet):
+    # TODO this is what you need to complete
+    
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    wallet = json.loads(inp)
+    check(wallet)
+############## DO NOT TOUCH AREA: END ###################
