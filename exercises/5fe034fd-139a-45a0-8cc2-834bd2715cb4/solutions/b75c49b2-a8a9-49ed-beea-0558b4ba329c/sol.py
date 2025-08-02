6d4198a609ffd7c47e110bbf915d9c8628920ec3
Agni Agni <y4nm45@gmail.com>
2025-08-02 18:58:00 +0100

    created file exercises/5fe034fd-139a-45a0-8cc2-834bd2715cb4/solutions/b75c49b2-a8a9-49ed-beea-0558b4ba329c/sol.py

---
+++ exercises/5fe034fd-139a-45a0-8cc2-834bd2715cb4/solutions/b75c49b2-a8a9-49ed-beea-0558b4ba329c/sol.py
@@ -0,0 +1,9 @@
+def broadcast():
+    # TODO this is what you need to complete
+    print("I’m gonna be a crypto millionaire!")
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    broadcast()
+############## DO NOT TOUCH AREA: END ###################
