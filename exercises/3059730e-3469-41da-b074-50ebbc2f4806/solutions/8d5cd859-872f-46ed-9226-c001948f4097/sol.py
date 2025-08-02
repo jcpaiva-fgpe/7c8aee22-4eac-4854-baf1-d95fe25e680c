b8b3c5a03ff597d132b2a5656d80f2c7484500bd
Agni Agni <y4nm45@gmail.com>
2025-08-02 18:58:53 +0100

    Removed exercises/3059730e-3469-41da-b074-50ebbc2f4806/solutions/8d5cd859-872f-46ed-9226-c001948f4097/sol.py

--- exercises/3059730e-3469-41da-b074-50ebbc2f4806/solutions/8d5cd859-872f-46ed-9226-c001948f4097/sol.py
+++
@@ -1,9 +0,0 @@
-def broadcast():
-    # TODO this is what you need to complete
-    print("I’m gonna be a crypto millionaire!")
-
-
-############## DO NOT TOUCH AREA: START #################
-if __name__ == '__main__':
-    broadcast()
-############## DO NOT TOUCH AREA: END ###################
