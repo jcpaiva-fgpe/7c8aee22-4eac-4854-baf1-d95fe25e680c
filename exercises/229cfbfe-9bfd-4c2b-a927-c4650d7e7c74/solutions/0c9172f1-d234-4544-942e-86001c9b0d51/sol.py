9c2b20ab8bf5e6256d884fed3545ecb6ea47af63
Agni Agni <y4nm45@gmail.com>
2025-08-02 19:35:36 +0100

    created file exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/solutions/0c9172f1-d234-4544-942e-86001c9b0d51/sol.py

---
+++ exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/solutions/0c9172f1-d234-4544-942e-86001c9b0d51/sol.py
@@ -0,0 +1,11 @@
+def coin():
+    # TODO this is what you need to complete
+    coin = "Ethereum"
+    gain_24h = 4.5
+    print(coin, "gained", gain_24h, "% today")
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    coin()
+############## DO NOT TOUCH AREA: END ###################
