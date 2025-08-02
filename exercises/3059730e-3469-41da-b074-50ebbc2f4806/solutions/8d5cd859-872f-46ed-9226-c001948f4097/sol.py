89bb5cf36315b0b0d95753020c1eb884d45e6b01
Agni Agni <y4nm45@gmail.com>
2025-08-02 18:48:49 +0100

    updated file exercises/3059730e-3469-41da-b074-50ebbc2f4806/solutions/8d5cd859-872f-46ed-9226-c001948f4097/sol.py

--- exercises/3059730e-3469-41da-b074-50ebbc2f4806/solutions/8d5cd859-872f-46ed-9226-c001948f4097/sol.py
+++ exercises/3059730e-3469-41da-b074-50ebbc2f4806/solutions/8d5cd859-872f-46ed-9226-c001948f4097/sol.py
@@ -1,11 +1,9 @@
-############## DO NOT TOUCH AREA: START #################
-
-if __name__ == '__main__':
-    broadcast()
-
-############## DO NOT TOUCH AREA: END ###################
-
-
 def broadcast():
     # TODO this is what you need to complete
     print("I’m gonna be a crypto millionaire!")
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    broadcast()
+############## DO NOT TOUCH AREA: END ###################
