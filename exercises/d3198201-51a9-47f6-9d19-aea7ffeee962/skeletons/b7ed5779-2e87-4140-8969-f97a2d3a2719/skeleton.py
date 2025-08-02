b0d0f0125ff62736a9933fe7f13cf78b33d0aba3
Agni Agni <y4nm45@gmail.com>
2025-08-02 20:00:21 +0100

    created file exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/skeletons/b7ed5779-2e87-4140-8969-f97a2d3a2719/skeleton.py

---
+++ exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/skeletons/b7ed5779-2e87-4140-8969-f97a2d3a2719/skeleton.py
@@ -0,0 +1,9 @@
+def wallet():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    wallet()
+############## DO NOT TOUCH AREA: END ###################
