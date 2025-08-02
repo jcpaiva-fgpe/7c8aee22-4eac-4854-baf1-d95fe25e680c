da4456f72da8ea17b9218ca1ceb232b48d0dd6e1
Agni Agni <y4nm45@gmail.com>
2025-08-02 20:22:39 +0100

    created file exercises/31bfccf0-4afc-4a57-b20d-b8c17b275b62/solutions/8e55f27c-7e49-4dcf-9d93-9497b0555ac9/sol.py

---
+++ exercises/31bfccf0-4afc-4a57-b20d-b8c17b275b62/solutions/8e55f27c-7e49-4dcf-9d93-9497b0555ac9/sol.py
@@ -0,0 +1,12 @@
+def calculation():
+    # TODO this is what you need to complete
+    bought = 30000
+    now = 68000
+    gain = now - bought
+    print(gain)
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    calculation()
+############## DO NOT TOUCH AREA: END ###################
