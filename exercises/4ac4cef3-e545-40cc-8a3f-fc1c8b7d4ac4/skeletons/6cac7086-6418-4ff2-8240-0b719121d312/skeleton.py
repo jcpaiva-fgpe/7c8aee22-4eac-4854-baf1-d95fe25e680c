e5bbe4338dd2cca8b8a86a937b0a65b3c58b533b
Agni Agni <y4nm45@gmail.com>
2025-08-03 10:12:33 +0100

    created file exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/skeletons/6cac7086-6418-4ff2-8240-0b719121d312/skeleton.py

---
+++ exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/skeletons/6cac7086-6418-4ff2-8240-0b719121d312/skeleton.py
@@ -0,0 +1,9 @@
+def tracking():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    tracking()
+############## DO NOT TOUCH AREA: END ###################
