1ba547b6d7223f02acd426c14e450276d296a104
Agni Agni <y4nm45@gmail.com>
2025-08-03 10:13:06 +0100

    created file exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/solutions/31b1c671-3cab-4d99-8d5e-93e8ce0f7399/sol.py

---
+++ exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/solutions/31b1c671-3cab-4d99-8d5e-93e8ce0f7399/sol.py
@@ -0,0 +1,11 @@
+def tracking():
+    # TODO this is what you need to complete
+    city = "Berlin"
+    local_rise = 0.9
+    print(city, "has warmed by", local_rise, "°C")
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    tracking()
+############## DO NOT TOUCH AREA: END ###################
