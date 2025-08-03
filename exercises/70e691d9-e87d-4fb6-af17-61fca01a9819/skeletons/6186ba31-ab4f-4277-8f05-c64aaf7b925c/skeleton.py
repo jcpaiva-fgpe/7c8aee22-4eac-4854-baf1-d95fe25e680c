724b916511c4c9bdec64aecae5a4928c928ddec7
Agni Agni <y4nm45@gmail.com>
2025-08-03 09:20:09 +0100

    created file exercises/70e691d9-e87d-4fb6-af17-61fca01a9819/skeletons/6186ba31-ab4f-4277-8f05-c64aaf7b925c/skeleton.py

---
+++ exercises/70e691d9-e87d-4fb6-af17-61fca01a9819/skeletons/6186ba31-ab4f-4277-8f05-c64aaf7b925c/skeleton.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def profits():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    print(profits())
+############## DO NOT TOUCH AREA: END ###################
