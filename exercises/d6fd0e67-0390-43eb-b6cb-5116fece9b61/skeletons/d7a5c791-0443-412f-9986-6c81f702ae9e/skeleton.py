6b05b7c28aeb8d508040b657846ada29801fad2b
Agni Agni <y4nm45@gmail.com>
2025-08-03 09:33:41 +0100

    created file exercises/d6fd0e67-0390-43eb-b6cb-5116fece9b61/skeletons/d7a5c791-0443-412f-9986-6c81f702ae9e/skeleton.py

---
+++ exercises/d6fd0e67-0390-43eb-b6cb-5116fece9b61/skeletons/d7a5c791-0443-412f-9986-6c81f702ae9e/skeleton.py
@@ -0,0 +1,15 @@
+import json
+import numpy as np
+
+def stats(array):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    array = json.loads(inp)
+    mean, std, maximum = stats(array)
+    print(mean, std, maximum)
+############## DO NOT TOUCH AREA: END ###################
