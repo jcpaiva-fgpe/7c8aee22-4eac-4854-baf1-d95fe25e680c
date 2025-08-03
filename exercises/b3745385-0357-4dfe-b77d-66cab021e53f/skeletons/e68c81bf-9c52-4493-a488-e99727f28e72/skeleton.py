4d29e60815335d52fa06120a8c7234c4c4325a49
Agni Agni <y4nm45@gmail.com>
2025-08-03 10:46:06 +0100

    created file exercises/b3745385-0357-4dfe-b77d-66cab021e53f/skeletons/e68c81bf-9c52-4493-a488-e99727f28e72/skeleton.py

---
+++ exercises/b3745385-0357-4dfe-b77d-66cab021e53f/skeletons/e68c81bf-9c52-4493-a488-e99727f28e72/skeleton.py
@@ -0,0 +1,9 @@
+def compare():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    compare()
+############## DO NOT TOUCH AREA: END ###################
