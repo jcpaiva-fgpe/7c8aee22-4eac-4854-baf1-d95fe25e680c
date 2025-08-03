e2c22275b18a74e1cb70c9ad3a3f67fda5b12ead
Agni Agni <y4nm45@gmail.com>
2025-08-03 08:53:58 +0100

    created file exercises/c653adc7-0f76-48ca-b35a-4c70b5d367fd/solutions/85f008ae-690c-445b-b7c0-c426bbee6c88/sol.py

---
+++ exercises/c653adc7-0f76-48ca-b35a-4c70b5d367fd/solutions/85f008ae-690c-445b-b7c0-c426bbee6c88/sol.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def track():
+    # TODO this is what you need to complete
+    days = np.array([1, 2, 3])
+    btc_prices = np.array([60000, 62000, 68000])
+    return days, btc_prices
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    days, btc_prices = track()
+    print(days, btc_prices)
+############## DO NOT TOUCH AREA: END ###################
