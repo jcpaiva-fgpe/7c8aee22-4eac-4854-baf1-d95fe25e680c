51064fbca1ac842448e18eb93fa68bfb80fd1ea7
Agni Agni <y4nm45@gmail.com>
2025-08-03 09:04:06 +0100

    created file exercises/e2142cec-6f50-4e0c-b0bf-e3d5d914a8dc/skeletons/d023f9d0-046a-4bf9-95f5-39658b940ad9/skeleton.py

---
+++ exercises/e2142cec-6f50-4e0c-b0bf-e3d5d914a8dc/skeletons/d023f9d0-046a-4bf9-95f5-39658b940ad9/skeleton.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def reshape():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    print(reshape())
+############## DO NOT TOUCH AREA: END ###################
