ca3b414d496ff9bae23503b8afbf1f2896bad15d
Agni Agni <y4nm45@gmail.com>
2025-08-03 10:56:14 +0100

    created file exercises/c674877a-d75c-4eb1-ae87-c5167fd8b796/skeletons/1d89a3df-1bf5-48c7-aef0-3c9ac89b7211/skeleton.py

---
+++ exercises/c674877a-d75c-4eb1-ae87-c5167fd8b796/skeletons/1d89a3df-1bf5-48c7-aef0-3c9ac89b7211/skeleton.py
@@ -0,0 +1,11 @@
+def decide(temp_rise):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    temp_rise = float(inp)
+    decide(temp_rise)
+############## DO NOT TOUCH AREA: END ###################
