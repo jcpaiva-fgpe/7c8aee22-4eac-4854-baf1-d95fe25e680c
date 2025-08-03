fa3543cbc316ac57aa808da9d19b218255ef63ad
Agni Agni <y4nm45@gmail.com>
2025-08-03 12:02:15 +0100

    created file exercises/48fa225c-e847-4380-9926-b9792564014d/skeletons/2e799090-7a8d-4e35-992d-eb1a579b45eb/skeleton.py

---
+++ exercises/48fa225c-e847-4380-9926-b9792564014d/skeletons/2e799090-7a8d-4e35-992d-eb1a579b45eb/skeleton.py
@@ -0,0 +1,12 @@
+import json
+
+# TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    values = json.loads(inp)
+    print(average_warming(values))
+############## DO NOT TOUCH AREA: END ###################
