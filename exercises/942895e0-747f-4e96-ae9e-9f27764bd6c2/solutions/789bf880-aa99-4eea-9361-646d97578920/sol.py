681241403c59927dc5319526f039f01ad5e754f8
Agni Agni <y4nm45@gmail.com>
2025-08-03 10:37:00 +0100

    created file exercises/942895e0-747f-4e96-ae9e-9f27764bd6c2/solutions/789bf880-aa99-4eea-9361-646d97578920/sol.py

---
+++ exercises/942895e0-747f-4e96-ae9e-9f27764bd6c2/solutions/789bf880-aa99-4eea-9361-646d97578920/sol.py
@@ -0,0 +1,11 @@
+def heat_impact():
+    # TODO this is what you need to complete
+    cities = ["Berlin", "Madrid", "Sydney"]
+    city_data = {"Berlin": 0.9, "Madrid": 1.2, "Sydney": 0.8}
+    print(cities, city_data)
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    heat_impact()
+############## DO NOT TOUCH AREA: END ###################
