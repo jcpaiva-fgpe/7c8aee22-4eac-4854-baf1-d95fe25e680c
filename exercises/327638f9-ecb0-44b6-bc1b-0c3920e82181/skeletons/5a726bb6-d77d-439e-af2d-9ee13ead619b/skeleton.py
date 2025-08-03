93919b3a3a2863c6bfae80a2c629772ee5b27f18
Agni Agni <y4nm45@gmail.com>
2025-08-03 11:51:04 +0100

    created file exercises/327638f9-ecb0-44b6-bc1b-0c3920e82181/skeletons/5a726bb6-d77d-439e-af2d-9ee13ead619b/skeleton.py

---
+++ exercises/327638f9-ecb0-44b6-bc1b-0c3920e82181/skeletons/5a726bb6-d77d-439e-af2d-9ee13ead619b/skeleton.py
@@ -0,0 +1,13 @@
+import json
+
+def sensors(sensor_array):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    sensor_array = json.loads(inp)
+    sensors(sensor_array)
+############## DO NOT TOUCH AREA: END ###################
