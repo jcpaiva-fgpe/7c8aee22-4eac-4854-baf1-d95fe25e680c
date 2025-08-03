138eee24d4bf8bdb2a12e5e22a765c44b401ea3c
Agni Agni <y4nm45@gmail.com>
2025-08-03 10:46:19 +0100

    created file exercises/b3745385-0357-4dfe-b77d-66cab021e53f/solutions/d57445eb-e8e4-4aac-a13d-7edbb2a8457f/sol.py

---
+++ exercises/b3745385-0357-4dfe-b77d-66cab021e53f/solutions/d57445eb-e8e4-4aac-a13d-7edbb2a8457f/sol.py
@@ -0,0 +1,12 @@
+def compare():
+    # TODO this is what you need to complete
+    global_rise = 1.2
+    local_rise = 0.9
+    diff = global_rise - local_rise
+    print(diff)
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    compare()
+############## DO NOT TOUCH AREA: END ###################
