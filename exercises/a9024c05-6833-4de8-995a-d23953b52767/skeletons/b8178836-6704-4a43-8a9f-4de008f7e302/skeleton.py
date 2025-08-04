5371afe50d7b7f2f70c3ae9e12db6cfad5ad846d
Agni Agni <y4nm45@gmail.com>
2025-08-04 13:19:01 +0100

    created file exercises/a9024c05-6833-4de8-995a-d23953b52767/skeletons/b8178836-6704-4a43-8a9f-4de008f7e302/skeleton.py

---
+++ exercises/a9024c05-6833-4de8-995a-d23953b52767/skeletons/b8178836-6704-4a43-8a9f-4de008f7e302/skeleton.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def co2():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    print(co2())
+############## DO NOT TOUCH AREA: END ###################
