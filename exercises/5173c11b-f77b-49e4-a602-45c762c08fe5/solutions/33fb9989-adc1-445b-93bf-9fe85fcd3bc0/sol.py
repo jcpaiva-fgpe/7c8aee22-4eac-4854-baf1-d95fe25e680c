32fcd484ba0dce9f4f567d06a0afedab17216cf7
Agni Agni <y4nm45@gmail.com>
2025-08-04 13:06:08 +0100

    created file exercises/5173c11b-f77b-49e4-a602-45c762c08fe5/solutions/33fb9989-adc1-445b-93bf-9fe85fcd3bc0/sol.py

---
+++ exercises/5173c11b-f77b-49e4-a602-45c762c08fe5/solutions/33fb9989-adc1-445b-93bf-9fe85fcd3bc0/sol.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def track():
+    # TODO this is what you need to complete
+    years = np.array([2000, 2005, 2010])
+    temps = np.array([0.5, 0.7, 1.0])
+    return years, temps
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    years, rise = track()
+    print(years, rise)
+############## DO NOT TOUCH AREA: END ###################
