04ad27dc43a010ed03ce586064ce3126a90253b3
Agni Agni <y4nm45@gmail.com>
2025-08-04 13:38:22 +0100

    created file exercises/97d5bc89-fd86-4f2f-8df6-c2da8379db4b/skeletons/bab1e617-cfe0-4539-9bc0-ff6f45f5c902/skeleton.py

---
+++ exercises/97d5bc89-fd86-4f2f-8df6-c2da8379db4b/skeletons/bab1e617-cfe0-4539-9bc0-ff6f45f5c902/skeleton.py
@@ -0,0 +1,15 @@
+import numpy as np
+import json
+
+def analyze(temperatures):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    temperatures = json.loads(inp)
+    mean, std_dev, variance = analyze(temperatures)
+    print(mean, std_dev, variance)
+############## DO NOT TOUCH AREA: END ###################
