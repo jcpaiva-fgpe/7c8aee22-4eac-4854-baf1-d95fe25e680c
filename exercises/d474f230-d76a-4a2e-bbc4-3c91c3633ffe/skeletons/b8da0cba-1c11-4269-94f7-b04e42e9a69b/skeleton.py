177c1c95822f768b66f6eeb7269020a5a0c62751
Agni Agni <y4nm45@gmail.com>
2025-08-04 13:31:05 +0100

    updated file exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/skeletons/b8da0cba-1c11-4269-94f7-b04e42e9a69b/skeleton.py

--- exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/skeletons/b8da0cba-1c11-4269-94f7-b04e42e9a69b/skeleton.py
+++ exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/skeletons/b8da0cba-1c11-4269-94f7-b04e42e9a69b/skeleton.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 def greenhouse_levels(co2_levels, methane_levels):
     # TODO this is what you need to complete
@@ -8,7 +9,7 @@ def greenhouse_levels(co2_levels, methane_levels):
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
     inp = input()
-    co2_levels = inp.slit(" ")[0]
-    methane_levels = inp.slit(" ")[1]
+    co2_levels = json.loads(inp.slit(" ")[0])
+    methane_levels = json.loads(inp.slit(" ")[1])
     print(greenhouse_levels(co2_levels, methane_levels))
 ############## DO NOT TOUCH AREA: END ###################
