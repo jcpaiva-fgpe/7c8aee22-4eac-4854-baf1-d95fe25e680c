39cca760a32e232f901de1f6fd74a109efdf128f
Agni Agni <y4nm45@gmail.com>
2025-08-04 13:28:26 +0100

    created file exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/skeletons/b8da0cba-1c11-4269-94f7-b04e42e9a69b/skeleton.py

---
+++ exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/skeletons/b8da0cba-1c11-4269-94f7-b04e42e9a69b/skeleton.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def greenhouse_levels(co2_levels, methane_levels):
+    # TODO this is what you need to complete
+    
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    co2_levels = inp.slit(" ")[0]
+    methane_levels = inp.slit(" ")[1]
+    print(greenhouse_levels(co2_levels, methane_levels))
+############## DO NOT TOUCH AREA: END ###################
