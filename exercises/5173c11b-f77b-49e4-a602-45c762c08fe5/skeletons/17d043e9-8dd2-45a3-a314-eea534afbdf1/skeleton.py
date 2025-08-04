b48a39f619181a64d8d9850c77fd49cc9b028b4f
Agni Agni <y4nm45@gmail.com>
2025-08-04 13:05:57 +0100

    created file exercises/5173c11b-f77b-49e4-a602-45c762c08fe5/skeletons/17d043e9-8dd2-45a3-a314-eea534afbdf1/skeleton.py

---
+++ exercises/5173c11b-f77b-49e4-a602-45c762c08fe5/skeletons/17d043e9-8dd2-45a3-a314-eea534afbdf1/skeleton.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def track():
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    years, rise = track()
+    print(years, rise)
+############## DO NOT TOUCH AREA: END ###################
