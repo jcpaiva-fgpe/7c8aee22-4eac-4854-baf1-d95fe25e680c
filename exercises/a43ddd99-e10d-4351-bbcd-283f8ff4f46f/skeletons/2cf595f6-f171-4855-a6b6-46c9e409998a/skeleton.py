d725dc474e7a104048a942dd56d986126ba89ac4
Agni Agni <y4nm45@gmail.com>
2025-08-18 15:10:11 +0100

    created file exercises/a43ddd99-e10d-4351-bbcd-283f8ff4f46f/skeletons/2cf595f6-f171-4855-a6b6-46c9e409998a/skeleton.py

---
+++ exercises/a43ddd99-e10d-4351-bbcd-283f8ff4f46f/skeletons/2cf595f6-f171-4855-a6b6-46c9e409998a/skeleton.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def weights(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    weights(inp)
+############## DO NOT TOUCH AREA: END ###################
