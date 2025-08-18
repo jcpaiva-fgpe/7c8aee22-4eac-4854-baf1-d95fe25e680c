1da0dea2cdb665f08fb66d09bdd17d3ffd6afaa1
Agni Agni <y4nm45@gmail.com>
2025-08-18 13:59:42 +0100

    created file exercises/11ab58f9-277a-491c-83d7-74181bec6629/skeletons/0350b6af-f601-4a7a-b779-6070a7adb7e5/skeleton.py

---
+++ exercises/11ab58f9-277a-491c-83d7-74181bec6629/skeletons/0350b6af-f601-4a7a-b779-6070a7adb7e5/skeleton.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def average(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    result = average(inp)
+    print(result)
+############## DO NOT TOUCH AREA: END ###################
