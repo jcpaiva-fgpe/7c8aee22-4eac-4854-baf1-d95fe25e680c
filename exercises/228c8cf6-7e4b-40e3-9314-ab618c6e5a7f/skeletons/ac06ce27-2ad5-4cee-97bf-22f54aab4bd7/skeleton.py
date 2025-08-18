18d9fff70aba2b5fcc332a3fd7c6328e35aff5eb
Agni Agni <y4nm45@gmail.com>
2025-08-18 14:10:12 +0100

    created file exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/skeletons/ac06ce27-2ad5-4cee-97bf-22f54aab4bd7/skeleton.py

---
+++ exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/skeletons/ac06ce27-2ad5-4cee-97bf-22f54aab4bd7/skeleton.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def grouping(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    result = grouping(inp)
+    print(result)
+############## DO NOT TOUCH AREA: END ###################
