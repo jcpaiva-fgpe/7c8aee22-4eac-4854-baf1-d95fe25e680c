3bde682b518de7cfd2dbb03b755e41871161735f
Agni Agni <y4nm45@gmail.com>
2025-08-18 15:59:38 +0100

    created file exercises/df26c7d6-38dd-44a1-93c5-b54de52ba262/solutions/cc5d7461-c9d3-4043-9e97-9f4f23ec222e/sol.py

---
+++ exercises/df26c7d6-38dd-44a1-93c5-b54de52ba262/solutions/cc5d7461-c9d3-4043-9e97-9f4f23ec222e/sol.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
+import matplotlib.pyplot as plt
+    
+def build_plot():
+    # TODO this is what you need to complete
+    plt.scatter(df["temp_anomaly"], df["sea_level"])
+    plt.title("Temp Anomaly vs Sea-Level Rise")
+    plt.xlabel("Anomaly (°C)")
+    plt.ylabel("Sea Level (mm)")
+    
+
+############## DO NOT TOUCH AREA: START #################
+def save_boxplot():
+    # Save plot
+    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    plt.savefig(file_path)
+    plt.close()  # Close the figure to free memory
+    return os.path.abspath(file_path)
+
+def main():
+    build_plot()
+    path = save_boxplot()
+    print(path)
+
+if __name__ == '__main__':
+    main()
+############## DO NOT TOUCH AREA: END ###################
