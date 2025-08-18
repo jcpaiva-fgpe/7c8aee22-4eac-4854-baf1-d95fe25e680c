55e15cfdcece733f9a1cbf067226a98a43aaec77
Agni Agni <y4nm45@gmail.com>
2025-08-18 16:37:28 +0100

    created file exercises/97049340-5d57-4941-86bb-5a2ae0949385/solutions/a4b07173-320d-4e7d-aa26-174b1d83d0ba/sol.py

---
+++ exercises/97049340-5d57-4941-86bb-5a2ae0949385/solutions/a4b07173-320d-4e7d-aa26-174b1d83d0ba/sol.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
+import matplotlib.pyplot as plt
+    
+def build_plot():
+    # TODO this is what you need to complete
+    plt.plot(df["Year"], df["temp_anomaly"],
+            color="red", linestyle="--", marker="o", label="Anomaly")
+    plt.title("Global Temp Anomaly")
+    plt.xlabel("Year")
+    plt.ylabel("Anomaly (°C)")
+    plt.grid(True)
+    plt.legend()
+    
+
+############## DO NOT TOUCH AREA: START #################
+def save_boxplot():
+    # Save plot
+    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    plt.savefig(file_path)
+    plt.close()  # Close the figure to free memory
+    return os.path.abspath(file_path)
+
+def main():
+    build_plot()
+    path = save_boxplot()
+    print(path)
+
+if __name__ == '__main__':
+    main()
+############## DO NOT TOUCH AREA: END ###################
