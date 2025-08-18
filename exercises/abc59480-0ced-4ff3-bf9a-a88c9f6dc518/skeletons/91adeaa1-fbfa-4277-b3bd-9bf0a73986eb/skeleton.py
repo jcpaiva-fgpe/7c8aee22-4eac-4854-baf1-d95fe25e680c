482601b4ab736ab990763785f6978e55f0c29066
Agni Agni <y4nm45@gmail.com>
2025-08-18 16:55:55 +0100

    created file exercises/abc59480-0ced-4ff3-bf9a-a88c9f6dc518/skeletons/91adeaa1-fbfa-4277-b3bd-9bf0a73986eb/skeleton.py

---
+++ exercises/abc59480-0ced-4ff3-bf9a-a88c9f6dc518/skeletons/91adeaa1-fbfa-4277-b3bd-9bf0a73986eb/skeleton.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
+import matplotlib.pyplot as plt
+    
+def build_plot():
+    # TODO this is what you need to complete
+
+    
+
+############## DO NOT TOUCH AREA: START #################
+def save_boxplot():
+    # Save plot
+    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    plt.savefig(file_path)
+    plt.close()  # Close the figure to free memory
+    return os.path.abspath(file_path)
+
+def main():
+    build_plot()
+    path = save_boxplot()
+    print(path)
+
+if __name__ == '__main__':
+    main()
+############## DO NOT TOUCH AREA: END ###################
