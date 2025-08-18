6a03cbc243263782335f18f36827445e51be1158
Agni Agni <y4nm45@gmail.com>
2025-08-18 16:48:36 +0100

    created file exercises/e8d01068-5a06-45ed-9718-bfe47852cbce/solutions/036d7fb1-cc7f-4f41-a304-11c83d5d5d57/sol.py

---
+++ exercises/e8d01068-5a06-45ed-9718-bfe47852cbce/solutions/036d7fb1-cc7f-4f41-a304-11c83d5d5d57/sol.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
+import matplotlib.pyplot as plt
+    
+def build_plot():
+    # TODO this is what you need to complete
+    top5 = df.nlargest(5, "Value")
+    plt.bar(top5["Coin"], top5["Value"])
+    plt.title("Top 5 Crypto Holdings")
+    plt.ylabel("USD Value")
+    
+
+############## DO NOT TOUCH AREA: START #################
+def save_boxplot():
+    # Save plot
+    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    plt.savefig(file_path)
+    plt.close()  # Close the figure to free memory
+    return os.path.abspath(file_path)
+
+def main():
+    build_plot()
+    path = save_boxplot()
+    print(path)
+
+if __name__ == '__main__':
+    main()
+############## DO NOT TOUCH AREA: END ###################
