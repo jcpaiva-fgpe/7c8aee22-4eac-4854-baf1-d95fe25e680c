c172db68f1bc0c6051d7b9e52af01cc760d3d8c9
Agni Agni <y4nm45@gmail.com>
2025-08-18 14:04:25 +0100

    created file exercises/4a0f0c83-cb16-4eac-aace-2e24ff1294a2/skeletons/5c049843-26a9-4aae-b1be-bbb1fa6955d4/skeleton.py

---
+++ exercises/4a0f0c83-cb16-4eac-aace-2e24ff1294a2/skeletons/5c049843-26a9-4aae-b1be-bbb1fa6955d4/skeleton.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def sort(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    result = sort(inp)
+    print(result)
+############## DO NOT TOUCH AREA: END ###################
