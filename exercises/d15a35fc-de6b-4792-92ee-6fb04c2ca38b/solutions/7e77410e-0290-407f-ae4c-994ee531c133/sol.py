3e9bfc1c2b0647d6ff17feb6cf12ffc464144e4b
Agni Agni <y4nm45@gmail.com>
2025-08-18 16:12:33 +0100

    created file exercises/d15a35fc-de6b-4792-92ee-6fb04c2ca38b/solutions/7e77410e-0290-407f-ae4c-994ee531c133/sol.py

---
+++ exercises/d15a35fc-de6b-4792-92ee-6fb04c2ca38b/solutions/7e77410e-0290-407f-ae4c-994ee531c133/sol.py
@@ -0,0 +1,29 @@
+import os
+import numpy as np
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
+import matplotlib.pyplot as plt
+    
+def build_plot():
+    # TODO this is what you need to complete
+    df.boxplot(column="CO2", by="Region")
+    plt.title("Emissions by Region")
+    plt.ylabel("CO₂ (Gt)")
+    plt.suptitle("")
+    
+
+############## DO NOT TOUCH AREA: START #################
+def save_boxplot():
+    # Save plot
+    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    plt.savefig(file_path)
+    plt.close()  # Close the figure to free memory
+    return os.path.abspath(file_path)
+
+def main():
+    build_plot()
+    path = save_boxplot()
+    print(path)
+
+if __name__ == '__main__':
+    main()
+############## DO NOT TOUCH AREA: END ###################
