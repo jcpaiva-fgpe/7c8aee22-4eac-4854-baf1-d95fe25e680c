b8fe3f7622ced302f7cbf039250d73163d83d578
Agni Agni <y4nm45@gmail.com>
2025-08-18 14:36:32 +0100

    created file exercises/07dfd20d-c529-4e00-a188-6bb9d471a6cd/skeletons/011254b5-1be8-4c6a-8216-de294f50f310/skeleton.py

---
+++ exercises/07dfd20d-c529-4e00-a188-6bb9d471a6cd/skeletons/011254b5-1be8-4c6a-8216-de294f50f310/skeleton.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def dataframe(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    result = dataframe(inp)
+############## DO NOT TOUCH AREA: END ###################
