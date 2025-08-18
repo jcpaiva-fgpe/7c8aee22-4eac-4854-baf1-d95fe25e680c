50db00db2c70f486ab8b8aa0ed3eb9689efb3159
Agni Agni <y4nm45@gmail.com>
2025-08-18 13:54:55 +0100

    created file exercises/3f5d1117-ede9-480f-ace2-45d8618721c0/solutions/6d96e6e9-8500-49c8-a38c-a8d2d258cd74/sol.py

---
+++ exercises/3f5d1117-ede9-480f-ace2-45d8618721c0/solutions/6d96e6e9-8500-49c8-a38c-a8d2d258cd74/sol.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def high_emission(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+if __name__ == '__main__':
+    inp = input()
+    result = high_emission(inp)
+    print(result)
+############## DO NOT TOUCH AREA: END ###################
