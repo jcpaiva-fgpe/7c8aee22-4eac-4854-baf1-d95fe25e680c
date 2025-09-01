929868e8815bee37d7539dd3caaa7b8d51d78511
Agni Agni <y4nm45@gmail.com>
2025-09-01 18:16:12 +0100

    updated file exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/skeletons/ac06ce27-2ad5-4cee-97bf-22f54aab4bd7/skeleton.py

--- exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/skeletons/ac06ce27-2ad5-4cee-97bf-22f54aab4bd7/skeleton.py
+++ exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/skeletons/ac06ce27-2ad5-4cee-97bf-22f54aab4bd7/skeleton.py
@@ -1,13 +1,22 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def grouping(path):
+def sort(path):
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_climate_5_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return os.path.abspath(file_path)
+
 if __name__ == '__main__':
     inp = input()
-    result = grouping(inp)
-    print(result)
+    df = sort(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
