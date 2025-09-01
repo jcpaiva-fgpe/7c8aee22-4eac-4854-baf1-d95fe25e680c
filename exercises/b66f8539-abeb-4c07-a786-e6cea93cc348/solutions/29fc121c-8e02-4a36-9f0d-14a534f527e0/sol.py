c6c884fff806752df13290e8236ba84a98534ac3
Agni Agni <y4nm45@gmail.com>
2025-09-01 19:54:28 +0100

    updated file exercises/b66f8539-abeb-4c07-a786-e6cea93cc348/solutions/29fc121c-8e02-4a36-9f0d-14a534f527e0/sol.py

--- exercises/b66f8539-abeb-4c07-a786-e6cea93cc348/solutions/29fc121c-8e02-4a36-9f0d-14a534f527e0/sol.py
+++ exercises/b66f8539-abeb-4c07-a786-e6cea93cc348/solutions/29fc121c-8e02-4a36-9f0d-14a534f527e0/sol.py
@@ -1,14 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
 def filtering(path):
     # TODO this is what you need to complete
-    profitable = df[df["CurrentPrice"] > df["BuyPrice"]]
+    df = pd.read_csv(path)
+    profitable = df[df["High"] > 100]
     return profitable
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_crypto_2_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return os.path.abspath(file_path)
+
 if __name__ == '__main__':
     inp = input()
-    result = filtering(inp)
-    print(result)
+    df = filtering(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
