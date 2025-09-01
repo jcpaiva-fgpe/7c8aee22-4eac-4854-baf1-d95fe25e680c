6378724c93c1b2aed9d67074c00fac615c59bea0
Agni Agni <y4nm45@gmail.com>
2025-09-01 18:21:11 +0100

    updated file exercises/2ef9941c-fe86-458b-a14d-d71045edd878/solutions/b03c4b08-9dad-4b18-816f-3e17f058cc6e/sol.py

--- exercises/2ef9941c-fe86-458b-a14d-d71045edd878/solutions/b03c4b08-9dad-4b18-816f-3e17f058cc6e/sol.py
+++ exercises/2ef9941c-fe86-458b-a14d-d71045edd878/solutions/b03c4b08-9dad-4b18-816f-3e17f058cc6e/sol.py
@@ -1,14 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
 def missing_values(path):
     # TODO this is what you need to complete
+    df = pd.read_csv(path)
     df["CO2"].fillna(df["CO2"].mean(), inplace=True)
-    print(df["CO2"].isna().sum())
+    return df
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_climate_2_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return os.path.abspath(file_path)
+
 if __name__ == '__main__':
     inp = input()
-    result = missing_values(inp)
-    print(result)
+    df = missing_values(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
