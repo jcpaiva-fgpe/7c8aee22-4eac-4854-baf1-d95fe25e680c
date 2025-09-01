96d061ae5f1872774bca050d1b43cadaf358c788
Agni Agni <y4nm45@gmail.com>
2025-09-01 20:18:15 +0100

    updated file exercises/c72f650f-431a-44b1-948e-27d7a38ed44e/skeletons/5f49d489-88d8-4343-9a06-6623e4e849dc/skeleton.py

--- exercises/c72f650f-431a-44b1-948e-27d7a38ed44e/skeletons/5f49d489-88d8-4343-9a06-6623e4e849dc/skeleton.py
+++ exercises/c72f650f-431a-44b1-948e-27d7a38ed44e/skeletons/5f49d489-88d8-4343-9a06-6623e4e849dc/skeleton.py
@@ -1,12 +1,22 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def summarize(path):
+def group(path):
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_crypto_5_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
 if __name__ == '__main__':
     inp = input()
-    summarize(inp)
+    df = group(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
