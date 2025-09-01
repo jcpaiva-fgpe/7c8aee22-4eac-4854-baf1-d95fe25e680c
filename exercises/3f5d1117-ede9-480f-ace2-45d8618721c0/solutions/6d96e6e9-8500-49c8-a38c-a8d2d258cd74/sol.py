921490ffbb1d1bada7e29fbb4dc7976986270120
Agni Agni <y4nm45@gmail.com>
2025-09-01 15:22:36 +0100

    updated file exercises/3f5d1117-ede9-480f-ace2-45d8618721c0/solutions/6d96e6e9-8500-49c8-a38c-a8d2d258cd74/sol.py

--- exercises/3f5d1117-ede9-480f-ace2-45d8618721c0/solutions/6d96e6e9-8500-49c8-a38c-a8d2d258cd74/sol.py
+++ exercises/3f5d1117-ede9-480f-ace2-45d8618721c0/solutions/6d96e6e9-8500-49c8-a38c-a8d2d258cd74/sol.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
 def high_emission(path):
     # TODO this is what you need to complete
-
+    df = pd.read_csv(path)
+    filtered = df[df["2040"] > 100]
+    return filtered
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_climate_2_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return os.path.abspath(file_path)
+
 if __name__ == '__main__':
     inp = input()
-    result = high_emission(inp)
-    print(result)
+    df = high_emission(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
