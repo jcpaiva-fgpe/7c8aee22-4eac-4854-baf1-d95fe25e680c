a295085a2ec41ea35a4dbc92688e1fd422c9b947
Agni Agni <y4nm45@gmail.com>
2025-09-01 18:15:59 +0100

    updated file exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/solutions/c5a4100f-7273-43d4-ac4d-277940cd81ac/sol.py

--- exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/solutions/c5a4100f-7273-43d4-ac4d-277940cd81ac/sol.py
+++ exercises/228c8cf6-7e4b-40e3-9314-ab618c6e5a7f/solutions/c5a4100f-7273-43d4-ac4d-277940cd81ac/sol.py
@@ -1,14 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def grouping(path):
+def sort(path):
     # TODO this is what you need to complete
-    grp = df.groupby("Region")["CO2"].agg(["sum", "mean"])
-    print(grp)
+    df = pd.read_csv(path)
+    grp = df.groupby("continent")[["2040", "2041", "2042", "2043", "2044"]].agg(["mean"])
+    return grp
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_climate_5_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return os.path.abspath(file_path)
+
 if __name__ == '__main__':
     inp = input()
-    result = grouping(inp)
-    print(result)
+    df = sort(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
