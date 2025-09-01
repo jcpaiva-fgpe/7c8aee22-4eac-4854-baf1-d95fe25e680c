964b5e3ac7ae1772d854a28340bb42fc9cbe096b
Agni Agni <y4nm45@gmail.com>
2025-09-01 20:09:56 +0100

    updated file exercises/649f1533-7800-45a4-b891-d85c402195b5/skeletons/aeb5a370-af23-4c8d-8504-d68f715695cf/skeleton.py

--- exercises/649f1533-7800-45a4-b891-d85c402195b5/skeletons/aeb5a370-af23-4c8d-8504-d68f715695cf/skeleton.py
+++ exercises/649f1533-7800-45a4-b891-d85c402195b5/skeletons/aeb5a370-af23-4c8d-8504-d68f715695cf/skeleton.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
 def sorting(path):
     # TODO this is what you need to complete
@@ -6,7 +9,14 @@ def sorting(path):
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_crypto_4_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
 if __name__ == '__main__':
     inp = input()
-    sorting(inp)
+    df = sorting(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
