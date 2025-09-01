1027d2788ba64dec9f7da57645417bd86acb0d39
Agni Agni <y4nm45@gmail.com>
2025-09-01 17:07:14 +0100

    updated file exercises/11ab58f9-277a-491c-83d7-74181bec6629/solutions/0a1b7445-5202-42e8-aa17-899f3d95e38f/sol.py

--- exercises/11ab58f9-277a-491c-83d7-74181bec6629/solutions/0a1b7445-5202-42e8-aa17-899f3d95e38f/sol.py
+++ exercises/11ab58f9-277a-491c-83d7-74181bec6629/solutions/0a1b7445-5202-42e8-aa17-899f3d95e38f/sol.py
@@ -2,13 +2,14 @@ import pandas as pd
 
 def average(path):
     # TODO this is what you need to complete
-    mean_co2 = df["CO2"].mean()
-    print("Average CO₂:", mean_co2)
+    df = pd.read_csv(path)
+    mean_co2 = df["2040"].mean()
+    return mean_co2
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
     inp = input()
-    result = average(inp)
-    print(result)
+    mean = average(inp)
+    print(mean)
 ############## DO NOT TOUCH AREA: END ###################
