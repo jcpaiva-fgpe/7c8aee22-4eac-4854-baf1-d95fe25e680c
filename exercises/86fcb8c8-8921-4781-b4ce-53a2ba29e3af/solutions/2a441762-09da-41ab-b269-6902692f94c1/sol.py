498bdf21498551a7623f0962a992812e8d5c8288
Agni Agni <y4nm45@gmail.com>
2025-09-01 20:02:23 +0100

    updated file exercises/86fcb8c8-8921-4781-b4ce-53a2ba29e3af/solutions/2a441762-09da-41ab-b269-6902692f94c1/sol.py

--- exercises/86fcb8c8-8921-4781-b4ce-53a2ba29e3af/solutions/2a441762-09da-41ab-b269-6902692f94c1/sol.py
+++ exercises/86fcb8c8-8921-4781-b4ce-53a2ba29e3af/solutions/2a441762-09da-41ab-b269-6902692f94c1/sol.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def total(path):
+def filtering(path):
     # TODO this is what you need to complete
-    df["Value"] = df["Amount"] * df["CurrentPrice"]
-    print(df["Value"].sum())
+    df = pd.read_csv(path)
+    df["Diff"] = df["High"] - df["Low"]
+    return df
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_crypto_3.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
 if __name__ == '__main__':
-    inp = input()
-    total(inp)
+    inp = "crypto.csv"
+    df = filtering(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
