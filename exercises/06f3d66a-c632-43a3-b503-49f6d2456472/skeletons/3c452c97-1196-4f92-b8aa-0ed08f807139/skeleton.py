5ec39f3c22b39d360a272dcc1232299fc0f88a77
Agni Agni <y4nm45@gmail.com>
2025-09-01 20:22:02 +0100

    updated file exercises/06f3d66a-c632-43a3-b503-49f6d2456472/skeletons/3c452c97-1196-4f92-b8aa-0ed08f807139/skeleton.py

--- exercises/06f3d66a-c632-43a3-b503-49f6d2456472/skeletons/3c452c97-1196-4f92-b8aa-0ed08f807139/skeleton.py
+++ exercises/06f3d66a-c632-43a3-b503-49f6d2456472/skeletons/3c452c97-1196-4f92-b8aa-0ed08f807139/skeleton.py
@@ -1,12 +1,22 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def missing_data(path):
+def missing_values(path):
     # TODO this is what you need to complete
-    
+
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_crypto_6_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
 if __name__ == '__main__':
     inp = input()
-    missing_data(inp)
+    df = missing_values(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
