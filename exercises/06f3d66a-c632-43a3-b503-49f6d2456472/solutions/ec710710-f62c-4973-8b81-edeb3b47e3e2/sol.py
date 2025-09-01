433d6d4204c37b9a684f4767076e1d2bd1ff13b0
Agni Agni <y4nm45@gmail.com>
2025-09-01 20:21:35 +0100

    updated file exercises/06f3d66a-c632-43a3-b503-49f6d2456472/solutions/ec710710-f62c-4973-8b81-edeb3b47e3e2/sol.py

--- exercises/06f3d66a-c632-43a3-b503-49f6d2456472/solutions/ec710710-f62c-4973-8b81-edeb3b47e3e2/sol.py
+++ exercises/06f3d66a-c632-43a3-b503-49f6d2456472/solutions/ec710710-f62c-4973-8b81-edeb3b47e3e2/sol.py
@@ -1,12 +1,24 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def missing_data(path):
+def group(path):
     # TODO this is what you need to complete
-    df["CurrentPrice"].fillna(df["CurrentPrice"].mean(), inplace=True)
+    df = pd.read_csv(path)
+    df["Open"].fillna(df["Open"].mean(), inplace=True)
+    return df
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_crypto_6_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
 if __name__ == '__main__':
     inp = input()
-    missing_data(inp)
+    df = group(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
