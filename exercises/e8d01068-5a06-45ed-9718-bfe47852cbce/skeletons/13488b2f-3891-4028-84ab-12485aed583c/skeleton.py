2dec2b4cd0557a66687b74b7078215f707758d6c
Agni Agni <y4nm45@gmail.com>
2025-09-02 09:25:41 +0100

    updated file exercises/e8d01068-5a06-45ed-9718-bfe47852cbce/skeletons/13488b2f-3891-4028-84ab-12485aed583c/skeleton.py

--- exercises/e8d01068-5a06-45ed-9718-bfe47852cbce/skeletons/13488b2f-3891-4028-84ab-12485aed583c/skeleton.py
+++ exercises/e8d01068-5a06-45ed-9718-bfe47852cbce/skeletons/13488b2f-3891-4028-84ab-12485aed583c/skeleton.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
+import pandas as pd
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 import matplotlib.pyplot as plt
     
-def build_plot():
+def build_plot(df):
     # TODO this is what you need to complete
 
     
@@ -11,16 +12,16 @@ def build_plot():
 ############## DO NOT TOUCH AREA: START #################
 def save_boxplot():
     # Save plot
-    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    file_path = 'barplot_' + str(np.random.randint(1000)) + '.png'
     plt.savefig(file_path)
     plt.close()  # Close the figure to free memory
     return os.path.abspath(file_path)
 
-def main():
-    build_plot()
+if __name__ == '__main__':
+    inp = input()
+    df = pd.read_csv(inp)
+    avg_prices = df.groupby('Name')['Close'].mean()
+    build_plot(avg_prices)
     path = save_boxplot()
     print(path)
-
-if __name__ == '__main__':
-    main()
 ############## DO NOT TOUCH AREA: END ###################
