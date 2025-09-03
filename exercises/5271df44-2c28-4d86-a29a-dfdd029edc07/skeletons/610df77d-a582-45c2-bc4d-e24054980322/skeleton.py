7ad29293d79be58efc1d4d8f8b2abc81af54b55f
Agni Agni <y4nm45@gmail.com>
2025-09-03 13:38:55 +0100

    updated file exercises/5271df44-2c28-4d86-a29a-dfdd029edc07/skeletons/610df77d-a582-45c2-bc4d-e24054980322/skeleton.py

--- exercises/5271df44-2c28-4d86-a29a-dfdd029edc07/skeletons/610df77d-a582-45c2-bc4d-e24054980322/skeleton.py
+++ exercises/5271df44-2c28-4d86-a29a-dfdd029edc07/skeletons/610df77d-a582-45c2-bc4d-e24054980322/skeleton.py
@@ -1,9 +1,11 @@
 import os
 import numpy as np
+import pandas as pd
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 import matplotlib.pyplot as plt
     
-def build_plot():
+def build_plot(china_emissions):
+    years = ['2040', '2041', '2042', '2043', '2044']
     # TODO this is what you need to complete
 
     
@@ -11,16 +13,18 @@ def build_plot():
 ############## DO NOT TOUCH AREA: START #################
 def save_boxplot():
     # Save plot
-    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    file_path = 'line_climate_' + str(np.random.randint(1000)) + '.png'
     plt.savefig(file_path)
     plt.close()  # Close the figure to free memory
     return os.path.abspath(file_path)
 
-def main():
-    build_plot()
+if __name__ == '__main__':
+    inp = input()
+    df = pd.read_csv(inp)
+    years = ['2040', '2041', '2042', '2043', '2044']
+    china_data = df[df['country'] == 'China'].iloc[0]
+    china_emissions = [china_data[year] for year in years]
+    build_plot(china_emissions)
     path = save_boxplot()
     print(path)
-
-if __name__ == '__main__':
-    main()
 ############## DO NOT TOUCH AREA: END ###################
