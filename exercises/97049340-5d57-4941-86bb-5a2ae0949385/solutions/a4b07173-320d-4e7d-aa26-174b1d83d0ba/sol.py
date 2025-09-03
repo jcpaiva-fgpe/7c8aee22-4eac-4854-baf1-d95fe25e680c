50c1eef023badf506845930aa7d741948f3f323a
Agni Agni <y4nm45@gmail.com>
2025-09-03 13:48:34 +0100

    updated file exercises/97049340-5d57-4941-86bb-5a2ae0949385/solutions/a4b07173-320d-4e7d-aa26-174b1d83d0ba/sol.py

--- exercises/97049340-5d57-4941-86bb-5a2ae0949385/solutions/a4b07173-320d-4e7d-aa26-174b1d83d0ba/sol.py
+++ exercises/97049340-5d57-4941-86bb-5a2ae0949385/solutions/a4b07173-320d-4e7d-aa26-174b1d83d0ba/sol.py
@@ -1,17 +1,18 @@
 import os
 import numpy as np
+import pandas as pd
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 import matplotlib.pyplot as plt
     
-def build_plot():
+def build_plot(portugal_emissions):
+    years = ['2040', '2041', '2042', '2043', '2044']
     # TODO this is what you need to complete
-    plt.plot(df["Year"], df["temp_anomaly"],
-            color="red", linestyle="--", marker="o", label="Anomaly")
-    plt.title("Global Temp Anomaly")
-    plt.xlabel("Year")
-    plt.ylabel("Anomaly (°C)")
+
+    plt.plot(years, portugal_emissions, color='blue')
+    plt.xlabel('Year')
+    plt.ylabel('CO2 Emissions (MtCO2e)')
+    plt.title('Portugal CO2 Emissions Over Time (2040-2044)')
     plt.grid(True)
-    plt.legend()
     
 
 ############## DO NOT TOUCH AREA: START #################
@@ -22,11 +23,13 @@ def save_boxplot():
     plt.close()  # Close the figure to free memory
     return os.path.abspath(file_path)
 
-def main():
-    build_plot()
+if __name__ == '__main__':
+    inp = input()
+    df = pd.read_csv(inp)
+    years = ['2040', '2041', '2042', '2043', '2044']
+    portugal_data = df[df['country'] == 'Portugal'].iloc[0]
+    portugal_emissions = [portugal_data[year] for year in years]
+    build_plot(portugal_emissions)
     path = save_boxplot()
     print(path)
-
-if __name__ == '__main__':
-    main()
 ############## DO NOT TOUCH AREA: END ###################
