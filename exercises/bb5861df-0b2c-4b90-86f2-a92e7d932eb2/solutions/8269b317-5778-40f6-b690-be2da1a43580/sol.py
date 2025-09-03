83a91efd3a90f41a6fa087094b8ac6d87c589a63
Agni Agni <y4nm45@gmail.com>
2025-09-03 12:49:53 +0100

    updated file exercises/bb5861df-0b2c-4b90-86f2-a92e7d932eb2/solutions/8269b317-5778-40f6-b690-be2da1a43580/sol.py

--- exercises/bb5861df-0b2c-4b90-86f2-a92e7d932eb2/solutions/8269b317-5778-40f6-b690-be2da1a43580/sol.py
+++ exercises/bb5861df-0b2c-4b90-86f2-a92e7d932eb2/solutions/8269b317-5778-40f6-b690-be2da1a43580/sol.py
@@ -1,33 +1,27 @@
 import os
 import numpy as np
+import pandas as pd
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 import matplotlib.pyplot as plt
     
-def build_plot():
+def build_plot(df):
     # TODO this is what you need to complete
-    btc_returns = np.random.normal(0, 1, 100)
-    eth_returns = btc_returns * 0.8 + np.random.normal(0, 0.5, 100)
+    plt.hist(df['High'], bins=20)
 
-    plt.scatter(btc_returns, eth_returns)
-    plt.title("BTC vs ETH Returns")
-    plt.xlabel("BTC Returns")
-    plt.ylabel("ETH Returns")
-    plt.grid(True)
-    
 
 ############## DO NOT TOUCH AREA: START #################
 def save_boxplot():
     # Save plot
-    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    file_path = 'histplot_' + str(np.random.randint(1000)) + '.png'
     plt.savefig(file_path)
     plt.close()  # Close the figure to free memory
     return os.path.abspath(file_path)
 
-def main():
-    build_plot()
+if __name__ == '__main__':
+    inp = input()
+    df = pd.read_csv(inp)
+    df = df[df["Name"] == "Ethereum"]
+    build_plot(df)
     path = save_boxplot()
     print(path)
-
-if __name__ == '__main__':
-    main()
 ############## DO NOT TOUCH AREA: END ###################
