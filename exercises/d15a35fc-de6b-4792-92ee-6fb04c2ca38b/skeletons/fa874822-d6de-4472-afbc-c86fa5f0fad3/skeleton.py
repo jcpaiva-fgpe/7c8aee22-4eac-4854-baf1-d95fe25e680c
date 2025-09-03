58c1c5ce5f233403134c2e524d9eb38da4e4caec
Agni Agni <y4nm45@gmail.com>
2025-09-03 13:46:01 +0100

    updated file exercises/d15a35fc-de6b-4792-92ee-6fb04c2ca38b/skeletons/fa874822-d6de-4472-afbc-c86fa5f0fad3/skeleton.py

--- exercises/d15a35fc-de6b-4792-92ee-6fb04c2ca38b/skeletons/fa874822-d6de-4472-afbc-c86fa5f0fad3/skeleton.py
+++ exercises/d15a35fc-de6b-4792-92ee-6fb04c2ca38b/skeletons/fa874822-d6de-4472-afbc-c86fa5f0fad3/skeleton.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
+import pandas as pd
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 import matplotlib.pyplot as plt
     
-def build_plot():
+def build_plot(df):
     # TODO this is what you need to complete
 
     
@@ -11,16 +12,15 @@ def build_plot():
 ############## DO NOT TOUCH AREA: START #################
 def save_boxplot():
     # Save plot
-    file_path = 'boxplot_' + str(np.random.randint(1000)) + '.png'
+    file_path = 'hist_climate_' + str(np.random.randint(1000)) + '.png'
     plt.savefig(file_path)
     plt.close()  # Close the figure to free memory
     return os.path.abspath(file_path)
 
-def main():
-    build_plot()
+if __name__ == '__main__':
+    inp = input()
+    df = pd.read_csv(inp)
+    build_plot(df)
     path = save_boxplot()
     print(path)
-
-if __name__ == '__main__':
-    main()
 ############## DO NOT TOUCH AREA: END ###################
