120fcf55c7d454886e8476d49b1ae7ec7d9df1b5
Agni Agni <y4nm45@gmail.com>
2025-09-04 16:21:04 +0100

    updated file exercises/a76bf11e-d1a1-4a7e-90c2-a51b3153b1cf/solutions/8b2e4b98-a056-42b8-836c-942d53d07745/sol.py

--- exercises/a76bf11e-d1a1-4a7e-90c2-a51b3153b1cf/solutions/8b2e4b98-a056-42b8-836c-942d53d07745/sol.py
+++ exercises/a76bf11e-d1a1-4a7e-90c2-a51b3153b1cf/solutions/8b2e4b98-a056-42b8-836c-942d53d07745/sol.py
@@ -1,14 +1,25 @@
 import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 
-def scaling(path):
+def sort(path):
     # TODO this is what you need to complete
-    df["CO2_scaled"] = df["CO2"].apply(lambda x: x / 10)
-    print(df[["CO2", "CO2_scaled"]].head())
+    df = pd.read_csv(path)
+    mean_2040 = df["2040"].mean()
+    df["2040_flag"] = df["2040"].apply(lambda x: "High" if x > mean_2040 else "Low")
+    return df
 
 
 ############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'pandas_climate_7.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
 if __name__ == '__main__':
-    inp = input()
-    result = scaling(inp)
-    print(result)
+    inp = "emissions.csv"
+    df = sort(inp)
+    path = save_df(df)
+    print(path)
 ############## DO NOT TOUCH AREA: END ###################
