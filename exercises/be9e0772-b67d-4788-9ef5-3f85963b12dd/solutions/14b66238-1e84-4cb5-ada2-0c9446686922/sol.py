31a656014a08acf27fd29b7575d1e564253e05f4
Agni Agni <y4nm45@gmail.com>
2025-09-04 13:19:28 +0100

    updated file exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/solutions/14b66238-1e84-4cb5-ada2-0c9446686922/sol.py

--- exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/solutions/14b66238-1e84-4cb5-ada2-0c9446686922/sol.py
+++ exercises/be9e0772-b67d-4788-9ef5-3f85963b12dd/solutions/14b66238-1e84-4cb5-ada2-0c9446686922/sol.py
@@ -1,15 +1,16 @@
 import json
 # TODO this is what you need to complete
-def portfolio(coins):
+def average(coins):
     total = 0
     for price in coins.values():
         total += price
-    return total
+    avg = total / len(coins)
+    return round(avg, 1)
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
     inp = input()
     data = json.loads(inp)
-    print(portfolio(data))
+    print(average(data))
 ############## DO NOT TOUCH AREA: END ###################
