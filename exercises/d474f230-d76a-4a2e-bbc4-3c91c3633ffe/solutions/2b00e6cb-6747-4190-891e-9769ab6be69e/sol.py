6ad203d1ad1fd2ddf476112bcfc1ed93a3487d7c
Agni Agni <y4nm45@gmail.com>
2025-09-04 13:51:07 +0100

    updated file exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/solutions/2b00e6cb-6747-4190-891e-9769ab6be69e/sol.py

--- exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/solutions/2b00e6cb-6747-4190-891e-9769ab6be69e/sol.py
+++ exercises/d474f230-d76a-4a2e-bbc4-3c91c3633ffe/solutions/2b00e6cb-6747-4190-891e-9769ab6be69e/sol.py
@@ -1,17 +1,17 @@
 import numpy as np
 import json
 
-def greenhouse_levels(co2_levels, methane_levels):
-    _co2_levels = np.array(co2_levels)
-    _methane_levels = np.array(methane_levels)
-    return (_co2_levels + _methane_levels)
+def combined_emission(source1, source2):
+    _source1 = np.array(source1)
+    _source2 = np.array(source2)
+    return (_source1 + _source2)
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
     inp = input().strip()
     parts = inp.split(" ")
-    co2_levels = json.loads(parts[0])
-    methane_levels = json.loads(parts[1])
-    print(greenhouse_levels(co2_levels, methane_levels))
+    source1 = json.loads(parts[0])
+    source2 = json.loads(parts[1])
+    print(combined_emission(source1, source2))
 ############## DO NOT TOUCH AREA: END ###################
