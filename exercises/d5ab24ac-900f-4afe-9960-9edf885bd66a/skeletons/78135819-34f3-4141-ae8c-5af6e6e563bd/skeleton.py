0e3b0492671aaa60ad6a467a3ceb7a2bc286c9e7
Agni Agni <y4nm45@gmail.com>
2025-09-04 19:06:42 +0100

    updated file exercises/d5ab24ac-900f-4afe-9960-9edf885bd66a/skeletons/78135819-34f3-4141-ae8c-5af6e6e563bd/skeleton.py

--- exercises/d5ab24ac-900f-4afe-9960-9edf885bd66a/skeletons/78135819-34f3-4141-ae8c-5af6e6e563bd/skeleton.py
+++ exercises/d5ab24ac-900f-4afe-9960-9edf885bd66a/skeletons/78135819-34f3-4141-ae8c-5af6e6e563bd/skeleton.py
@@ -3,7 +3,7 @@ import numpy as np
 os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
 import matplotlib.pyplot as plt
     
-def build_plot():
+def build_plot(sizes):
     # TODO this is what you need to complete
 
     
@@ -20,7 +20,8 @@ if __name__ == '__main__':
     inp = input()
     df = pd.read_csv(inp)
     volume_share = df.groupby('Name')['Volume'].sum()
-    build_plot(volume_share)
+    sizes = [volume_share['Bitcoin'], volume_share['Ethereum']]
+    build_plot(sizes)
     path = save_boxplot()
     print(path)
 ############## DO NOT TOUCH AREA: END ###################
