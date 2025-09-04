38c166984b70976739011c5b7b2336e4163b3224
Agni Agni <y4nm45@gmail.com>
2025-09-04 16:05:08 +0100

    updated file exercises/11ab58f9-277a-491c-83d7-74181bec6629/solutions/0a1b7445-5202-42e8-aa17-899f3d95e38f/sol.py

--- exercises/11ab58f9-277a-491c-83d7-74181bec6629/solutions/0a1b7445-5202-42e8-aa17-899f3d95e38f/sol.py
+++ exercises/11ab58f9-277a-491c-83d7-74181bec6629/solutions/0a1b7445-5202-42e8-aa17-899f3d95e38f/sol.py
@@ -4,12 +4,15 @@ def average(path):
     # TODO this is what you need to complete
     df = pd.read_csv(path)
     mean_co2 = df["2040"].mean()
-    return mean_co2
+    median_co2 = df["2040"].median()
+    min_co2 = df["2040"].min()
+    max_co2 = df["2040"].max()
+    return mean_co2, median_co2, min_co2, max_co2
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
     inp = input()
-    mean = average(inp)
-    print(mean)
+    mean, median, mi, ma = average(inp)
+    print(mean, median, mi, ma)
 ############## DO NOT TOUCH AREA: END ###################
