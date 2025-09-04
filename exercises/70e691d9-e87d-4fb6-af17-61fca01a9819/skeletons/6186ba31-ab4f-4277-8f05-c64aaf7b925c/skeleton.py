556c533299b4ce320ce148c1a5c0e18fb806ca47
Agni Agni <y4nm45@gmail.com>
2025-09-04 15:22:02 +0100

    updated file exercises/70e691d9-e87d-4fb6-af17-61fca01a9819/skeletons/6186ba31-ab4f-4277-8f05-c64aaf7b925c/skeleton.py

--- exercises/70e691d9-e87d-4fb6-af17-61fca01a9819/skeletons/6186ba31-ab4f-4277-8f05-c64aaf7b925c/skeleton.py
+++ exercises/70e691d9-e87d-4fb6-af17-61fca01a9819/skeletons/6186ba31-ab4f-4277-8f05-c64aaf7b925c/skeleton.py
@@ -1,11 +1,16 @@
 import numpy as np
+import json
 
-def profits():
+def profits(bitcoin, ethereum):
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
-    print(profits())
+    inp = input().strip()
+    parts = inp.split(" ")
+    bitcoin = json.loads(parts[0])
+    ethereum = json.loads(parts[1])
+    print(profits(bitcoin, ethereum))
 ############## DO NOT TOUCH AREA: END ###################
