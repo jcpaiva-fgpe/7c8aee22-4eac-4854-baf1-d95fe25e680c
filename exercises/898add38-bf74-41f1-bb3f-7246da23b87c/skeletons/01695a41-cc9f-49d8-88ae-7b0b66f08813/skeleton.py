1b4d2f0bf02969d3ca0e31b0780af3719d95da21
Agni Agni <y4nm45@gmail.com>
2025-09-04 13:11:57 +0100

    updated file exercises/898add38-bf74-41f1-bb3f-7246da23b87c/skeletons/01695a41-cc9f-49d8-88ae-7b0b66f08813/skeleton.py

--- exercises/898add38-bf74-41f1-bb3f-7246da23b87c/skeletons/01695a41-cc9f-49d8-88ae-7b0b66f08813/skeleton.py
+++ exercises/898add38-bf74-41f1-bb3f-7246da23b87c/skeletons/01695a41-cc9f-49d8-88ae-7b0b66f08813/skeleton.py
@@ -1,13 +1,13 @@
 import json
 
-def check(wallet):
+def check(sweeps):
     # TODO this is what you need to complete
-    
+
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
     inp = input()
-    wallet = json.loads(inp)
-    check(wallet)
+    sweeps = json.loads(inp)
+    check(sweeps)
 ############## DO NOT TOUCH AREA: END ###################
