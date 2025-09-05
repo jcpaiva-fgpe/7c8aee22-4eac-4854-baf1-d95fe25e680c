1e3c02ebf6c65512b19c07050950e0a6d84cab6c
Agni Agni <y4nm45@gmail.com>
2025-09-05 16:15:58 +0100

    updated file exercises/97d5bc89-fd86-4f2f-8df6-c2da8379db4b/skeletons/bab1e617-cfe0-4539-9bc0-ff6f45f5c902/skeleton.py

--- exercises/97d5bc89-fd86-4f2f-8df6-c2da8379db4b/skeletons/bab1e617-cfe0-4539-9bc0-ff6f45f5c902/skeleton.py
+++ exercises/97d5bc89-fd86-4f2f-8df6-c2da8379db4b/skeletons/bab1e617-cfe0-4539-9bc0-ff6f45f5c902/skeleton.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 
-def analyze(warmings):
+def stats(warmings):
     # TODO this is what you need to complete
 
 
@@ -10,6 +10,6 @@ def analyze(warmings):
 if __name__ == '__main__':
     inp = input()
     warmings = json.loads(inp)
-    mean, std_dev, variance = analyze(warmings)
+    mean, std_dev, variance = stats(warmings)
     print(mean, std_dev, variance)
 ############## DO NOT TOUCH AREA: END ###################
