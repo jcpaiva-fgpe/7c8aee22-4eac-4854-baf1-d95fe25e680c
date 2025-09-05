8b24e5eaf303178528a86b90ddd109000efd8713
Agni Agni <y4nm45@gmail.com>
2025-09-05 15:27:07 +0100

    updated file exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/skeletons/6cac7086-6418-4ff2-8240-0b719121d312/skeleton.py

--- exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/skeletons/6cac7086-6418-4ff2-8240-0b719121d312/skeleton.py
+++ exercises/4ac4cef3-e545-40cc-8a3f-fc1c8b7d4ac4/skeletons/6cac7086-6418-4ff2-8240-0b719121d312/skeleton.py
@@ -1,9 +1,9 @@
-def tracking():
+def status_line():
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
-    tracking()
+    status_line()
 ############## DO NOT TOUCH AREA: END ###################
