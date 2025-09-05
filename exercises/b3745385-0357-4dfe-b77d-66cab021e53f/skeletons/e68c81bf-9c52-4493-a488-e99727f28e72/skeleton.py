c5c84b3f93de1db05ac2e0a1e78025e598137023
Agni Agni <y4nm45@gmail.com>
2025-09-05 15:27:48 +0100

    updated file exercises/b3745385-0357-4dfe-b77d-66cab021e53f/skeletons/e68c81bf-9c52-4493-a488-e99727f28e72/skeleton.py

--- exercises/b3745385-0357-4dfe-b77d-66cab021e53f/skeletons/e68c81bf-9c52-4493-a488-e99727f28e72/skeleton.py
+++ exercises/b3745385-0357-4dfe-b77d-66cab021e53f/skeletons/e68c81bf-9c52-4493-a488-e99727f28e72/skeleton.py
@@ -1,9 +1,9 @@
-def compare():
+def global_vs_local():
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
-    compare()
+    global_vs_local()
 ############## DO NOT TOUCH AREA: END ###################
