de9f150b264935cae5004f720f9c542c4f5f038e
Agni Agni <y4nm45@gmail.com>
2025-09-05 15:46:46 +0100

    updated file exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/skeletons/b7ed5779-2e87-4140-8969-f97a2d3a2719/skeleton.py

--- exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/skeletons/b7ed5779-2e87-4140-8969-f97a2d3a2719/skeleton.py
+++ exercises/d3198201-51a9-47f6-9d19-aea7ffeee962/skeletons/b7ed5779-2e87-4140-8969-f97a2d3a2719/skeleton.py
@@ -1,9 +1,9 @@
-def wallet():
+def assets():
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
-    wallet()
+    assets()
 ############## DO NOT TOUCH AREA: END ###################
