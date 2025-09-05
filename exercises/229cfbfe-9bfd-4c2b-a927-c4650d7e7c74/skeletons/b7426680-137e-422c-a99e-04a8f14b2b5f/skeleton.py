02ef867529ae76f77c2fcc138afbea51044aa6c3
Agni Agni <y4nm45@gmail.com>
2025-09-05 15:45:21 +0100

    updated file exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/skeletons/b7426680-137e-422c-a99e-04a8f14b2b5f/skeleton.py

--- exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/skeletons/b7426680-137e-422c-a99e-04a8f14b2b5f/skeleton.py
+++ exercises/229cfbfe-9bfd-4c2b-a927-c4650d7e7c74/skeletons/b7426680-137e-422c-a99e-04a8f14b2b5f/skeleton.py
@@ -1,9 +1,9 @@
-def coin():
+def status_line():
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
-    coin()
+    status_line()
 ############## DO NOT TOUCH AREA: END ###################
