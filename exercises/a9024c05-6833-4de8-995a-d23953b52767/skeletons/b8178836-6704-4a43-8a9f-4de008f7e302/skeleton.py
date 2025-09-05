5d7c0dea2c3737ef4fcfccecc494d0cb5dcddbff
Agni Agni <y4nm45@gmail.com>
2025-09-05 16:13:26 +0100

    updated file exercises/a9024c05-6833-4de8-995a-d23953b52767/skeletons/b8178836-6704-4a43-8a9f-4de008f7e302/skeleton.py

--- exercises/a9024c05-6833-4de8-995a-d23953b52767/skeletons/b8178836-6704-4a43-8a9f-4de008f7e302/skeleton.py
+++ exercises/a9024c05-6833-4de8-995a-d23953b52767/skeletons/b8178836-6704-4a43-8a9f-4de008f7e302/skeleton.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-def co2():
+def reshape():
     # TODO this is what you need to complete
 
 
 
 ############## DO NOT TOUCH AREA: START #################
 if __name__ == '__main__':
-    print(co2())
+    print(reshape())
 ############## DO NOT TOUCH AREA: END ###################
