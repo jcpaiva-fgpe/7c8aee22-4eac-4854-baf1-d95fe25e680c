ab10314d42a719220e0ff0738dc3a441da507a90
Agni Agni <y4nm45@gmail.com>
2025-09-10 18:17:35 +0100

    created file exercises/b8a380d0-0378-421f-84b4-9934bed9b67d/skeletons/846e7355-d7c7-4e02-bbbc-c24c9c58b4c4/skeleton.py

---
+++ exercises/b8a380d0-0378-421f-84b4-9934bed9b67d/skeletons/846e7355-d7c7-4e02-bbbc-c24c9c58b4c4/skeleton.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import os
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/.tmp"
+
+def applying(path):
+    # TODO this is what you need to complete
+
+
+
+############## DO NOT TOUCH AREA: START #################
+def save_df(df):
+    file_path = 'ml_climate_1_' + str(np.random.randint(1000)) + '.csv'
+    df.to_csv(file_path, index=False)
+    return file_path
+
+if __name__ == '__main__':
+    inp = input()
+    df = applying(inp)
+    path = save_df(df)
+    print(path)
+############## DO NOT TOUCH AREA: END ###################
